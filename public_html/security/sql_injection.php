9ed5d5e4a8fbe287643bb35a8d01262a5ffb97b1
keito-isurugi <ja224kg@gmail.com>
2023-03-15 10:01:55 +0900

    sqlインジェクション

--- public_html/security/sql_injection.php
+++ public_html/security/sql_injection.php
@@ -3,42 +3,26 @@
 	
 	// 取得
 	$db = getDb();
-	$stmt = $db->prepare('SELECT * FROM users');
+	$stmt = $db->prepare('SELECT * FROM users WHERE id ='.$_POST['id']);
 	$res = $stmt->execute();
 	if($res) {
-		$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		$user = $stmt->fetch(PDO::FETCH_ASSOC);
 	}
 
-	// 登録
-	if( !empty($_POST['name']) ) {
-		$db = getDb();
-		$stmt = $db->prepare('INSERT INTO users (name, email) VALUES(:name, :email)');
-		$stmt->bindParam( ':name', $_POST['name'], PDO::PARAM_STR);
-		$stmt->bindParam( ':email', $_POST['email'], PDO::PARAM_STR);
-		$res = $stmt->execute();
+	// DELETE
+	// delete from users where id = 1;
 	
-		if($res) {
-			$stmt = null;
-			header('Location: ./sql_injection.php');
-			exit;
-		}
-	}
-
 ?>
 
 <form action="" method="POST">
-	<label for="name">name</label>
-	<input id="name" type="text" name="name">
-	<label for="email">email</label>
-	<input id="email" type="email" name="email">
+	<label for="id">id</label>
+	<input id="id" type="text" name="id">
 	<button>送信</button>
 </form>
 <div>
-	<?php foreach($users as $user) { ?>
-		<div>
-			<span>No: <?php echo $user['id'] ?>　</span>
-			<span>name: <?php echo $user['name'] ?>　</span>
-			<span>email: <?php echo $user['email'] ?>　</span>
-		</div>
-	<?php } ?>
+	<div>
+		<span>No: <?php echo $user['id'] ?>　</span>
+		<span>name: <?php echo $user['name'] ?>　</span>
+		<span>email: <?php echo $user['email'] ?>　</span>
+	</div>
 </div>
