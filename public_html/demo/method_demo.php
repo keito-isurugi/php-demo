b960d1c1bab8e4796f68f83b47c198c2096ec4e4
KeitoIsurugi <isurugi@csplan.jp>
2023-06-27 23:52:49 +0900

    error_log

--- public_html/demo/method_demo.php
+++ public_html/demo/method_demo.php
@@ -7,3 +7,5 @@ class Person{
 }
 // 動的に呼び出したいときはforward_static_call_arrayが便利
 echo forward_static_call_array(['Person', 'name'], ['yamada']);
+
+error_log("エラーメッセージaaa", 3, "/home/homepage/log/error.log");
---
+++ public_html/demo/script.php
@@ -0,0 +1,2 @@
+<?php 
+error_log("クーロンです", 3, "/home/homepage/log/error.log");
