034185a06b87ae9140eb9badb6778e586a7ff8dd
KeitoIsurugi <ja224kg@gmail.com>
2023-10-15 22:45:38 +0900

    commit

---
+++ public_html/tyozetu_sw/part6/src/Core/NumberConverter.php
@@ -0,0 +1,21 @@
+<?php
+namespace PublicHtml\tyozetu_sw\part6\src\Core;
+
+class NumberConverter
+{
+  /**
+   * @param ReplaceRuleInterface[] $rules
+   */
+  public function __construct(protected array $rules){}
+
+  public function convert(int $n): string
+  {
+    $carry = "";
+    foreach($this->rules as $rule) {
+      if($rule->match($carry, $n)){
+        $carry = $rule->apply($carry, $n);
+      }
+    }
+    return $carry;
+  }
+}
